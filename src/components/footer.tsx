19e9e9f7be0ea1a1315df85d1d0cdceab96df3ed
Tanishq Srivastava <yashu_comp@hotmail.com>
2024-08-23 17:08:20 +0530

    footer fixes double scrollbar fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import '/src/styles/slider1.css';
-
+import styles from '/src/styles/FollowPageCard.module.css';
 
 
 export default function Footer() {
@@ -9,17 +9,17 @@ export default function Footer() {
         <div className="mt-4">
           <img src='images/general/logowhite.png' className="footImg"></img>
           <div className="buttonsAtEnd">
-            <div className="bg-black hover:bg-white" style={{ margin: '0px', width: '20px', height: '20px' }}></div>
-            <div className="bg-black hover:bg-white" style={{ margin: '0px', width: '20px', height: '20px' }}></div>
-            <div className="bg-black hover:bg-white" style={{ margin: '0px', width: '20px', height: '20px' }}></div>
+            <button className={`${styles.socialButton} ${styles.instagram}`} aria-label="Instagram"></button>
+            <button className={`${styles.socialButton} ${styles.facebook}`} aria-label="Facebook"></button>
+            <button className={`${styles.socialButton} ${styles.twitter}`} aria-label="Twitter"></button>
           </div>
         </div>
         <div className="mt-4">
           <p className="textEnd text-left text-xl" style={{ color: 'white' }}>Sign Up</p>
           <p className="textEnd text-left">Want to keep informed? Join our infrequent mailing list, and you will never miss out on our upcoming events.</p>
-          <span style={{ width: '100%' }}>
+          <span className="flex flex-row " style={{ width: '100%', marginBottom: '0px', height: '25px' }}>
             <input type="text" style={{ width: '80%', marginLeft: '0px', marginRight: '0px' }}></input>
-            <button className="bg-black hover:bg-white" style={{ height: '40px', width: '20%', marginLeft: '0px', marginRight: '0px' }}>Submit</button></span>
+            <button className="bg-black px-2 hover:bg-white" style={{ height: '25px', marginLeft: '0px', marginRight: '0px' }}>Submit</button></span>
         </div>
         <div className="ieeecs mt-8" style={{ marginLeft: '100px' }}>
           <img src="images/general/ieeecslogo.png" className="footImg"></img>
